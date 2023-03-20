88abfa2bfc7a4e23cb07792f0e903cf1e301446c
Mamadou <mamadou.thiongane@grenoble-inp.org>
2023-03-20 10:24:38 +0100

    addi, lui

--- program/autotest/addi.s
+++ program/autotest/addi.s
@@ -1,8 +1,8 @@
 # TAG = addi
 	.text
 
-	addi x31, x0, 1   # Test addition immédiate avec une valeur quelconque
-	addi x31, x0, 5	  # Test addition immédiate avec une valeur quelconque
+	addi x31, x0, 1   # r31 = r0 + 1
+	addi x31, x0, 5	  # r31 = r0 + 5
 
 	# max_cycle 50
 	# pout_start
