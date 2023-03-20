8ca387bdb4568e26a3c71670dfd32132586e5282
dhouibw <walid.dhouib@grenoble-inp.org>
2023-03-20 16:57:07 +0100

    x

--- program/autotest/sub.s
+++ program/autotest/sub.s
@@ -1,10 +1,10 @@
 # TAG = sub
 	.text
 
-	addi x1, x0, 0x123   # r1 = r0 + 0x12345
-    addi x2, x0, 0x456   # r2 = r0 + 0x56789
-	sub x31, x1, x0   # r31 = r0 + r1
-	sub x31, x2, x1   # r31 = r1 + r2
+	addi x1, x0, 0x123   # r1 = r0 + 0x123
+    addi x2, x0, 0x456   # r2 = r0 + 0x456
+	sub x31, x1, x0   # r31 = r1 - r0
+	sub x31, x2, x1   # r31 = r2 - r1
     lui x0, 0
 
 	# max_cycle 50
